f22e2191cb8baf1d487088fafbd5cc4aff8ee677
ehdrnr333 <ehdrnr333@naver.com>
2017-11-08 22:59:47 +0900

    데이터를 이용해서 길 안내용 자료로 가공

--- app/src/main/java/com/example/user/yellowblockdetect/MainActivity.java
+++ app/src/main/java/com/example/user/yellowblockdetect/MainActivity.java
@@ -5,37 +5,68 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.Toast;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "opencv";
-    private CameraBridgeViewBase mOpenCvCameraView;
+    private JavaCameraView mOpenCvCameraView;
     private Mat matInput;
     private Mat matResult;
-
-    public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
-
+    public native int[] ProcessFrame(long matAddrInput, long matAddrResult);
     static {
         System.loadLibrary("opencv_java3");
         System.loadLibrary("native-lib");
     }
 
+    int[] jni_data = new int[40+2];
+    String log_txt = "";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!hasPermissions(PERMISSIONS)) {
+
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        }
+
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.camera);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        mMainHandler = new SendMassgeHandler();
+    }
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -52,116 +83,175 @@ public class MainActivity extends AppCompatActivity
         }
     };
 
+    // <핸들러>
+    private static final int SEND_THREAD_INFOMATION = 0;
+    private static final int SEND_THREAD_STOP_MESSAGE = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private SendMassgeHandler mMainHandler = null;
+    class SendMassgeHandler extends Handler {
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.activity_main);
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //퍼미션 상태 확인
-            if (!hasPermissions(PERMISSIONS)) {
-
-                //퍼미션 허가 안되어있다면 사용자에게 요청
-                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            switch (msg.what) {
+                // ** 여기에 장애인 알림 서비스 메소드 추가 **
+                case SEND_THREAD_INFOMATION:
+                    Toast.makeText(getApplicationContext(), "slope = " + msg.arg2 + "  \n" + msg.obj, Toast.LENGTH_SHORT).show();
+                    //debugText.setText("현재각도 = " + msg.arg1 + "\n" +"slope = " + msg.arg2 + "  \n" + msg.obj);
+                    break;
+                case SEND_THREAD_STOP_MESSAGE:
+                    Toast.makeText(getApplicationContext(), "Thread가 중지 되었습니다.", Toast.LENGTH_LONG).show();
+                    //debugText.setText("Thread가 중지 되었습니다.");
+                    break;
+                default:
+                    break;
             }
         }
+    };
+    // </핸들러>
 
-        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
-        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-    }
-
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-
-        if (!OpenCVLoader.initDebug()) {
-            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
-        } else {
-            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-    }
-
-    public void onDestroy() {
-        super.onDestroy();
-
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
+    // <C++ 영상처리 함수 호출 영역>
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         matInput = inputFrame.rgba();
-
         if ( matResult != null ) matResult.release();
         matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
+        jni_data = ProcessFrame(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
 
-        ConvertRGBtoGray(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
+        // 핸들러에 메세지 가공해서 전달하는 영역
 
+        if(jni_data[0] > 0) {
+            int x = matInput.cols();
+            int y = matInput.rows()/2;
+            double[] slope = new double[20];
+            double averageSlope = 0.0f;
+            boolean check = false;
+            for (int i = 0; i < jni_data[0]; i++) {
+                if(jni_data[i * 2 + 1] >= x * 0.9)
+                    check = true;
+                if (jni_data[i * 2 + 2] != y)
+                    slope[i] = ((double) jni_data[i * 2 + 1] - x) / (jni_data[i * 2 + 2] - y);
+                else
+                    slope[i] = jni_data[i * 2 + 1] - x;
+                averageSlope += slope[i];
+            }
+
+            averageSlope /= jni_data[0];
+
+            Arrays.sort(slope, 0, jni_data[0]);
+
+            Message msg = mMainHandler.obtainMessage();
+            msg.what = SEND_THREAD_INFOMATION;
+            msg.arg1 = 30;
+            msg.arg2 = (int)jni_data[41];
+
+            final double[] angle = {7.111, 4, 1.1851};
+            String hi;
+            if (slope[0] * slope[jni_data[0] - 1] < 0)
+            {
+                hi = new String("전방 ["+averageSlope+"]");
+                if(angle[1] <= averageSlope && averageSlope < angle[0])
+                    hi += "1시 방향";
+                else if(angle[2] <= averageSlope && averageSlope < angle[1])
+                    hi += "2시 방향";
+                else if(averageSlope < angle[2])
+                    hi += "3시 방향";
+                else if(-angle[1] > averageSlope && averageSlope >= -angle[0])
+                    hi += "11시 방향";
+                else if(-angle[2] > averageSlope && averageSlope >= -angle[1])
+                    hi += "10시 방향";
+                else if(averageSlope >= -angle[2])
+                    hi += "9시 방향";
+                msg.obj = hi;
+            }
+            else if (slope[0] > 0)
+            {
+                hi = new String("오른쪽 ["+averageSlope+"]");
+                if(angle[1] <= averageSlope && averageSlope < angle[0])
+                    hi += "1시 방향";
+                else if(angle[2] <= averageSlope && averageSlope < angle[1])
+                    hi += "2시 방향";
+                else if(averageSlope < angle[2])
+                    hi += "3시 방향";
+                msg.obj = hi;
+            }
+            else
+            {
+                hi = new String("왼쪽 ["+averageSlope+"]");
+                if(-angle[1] > averageSlope && averageSlope >= -angle[0])
+                    hi += "11시 방향";
+                else if(-angle[2] > averageSlope && averageSlope >= -angle[1])
+                    hi += "10시 방향";
+                else if(averageSlope >= -angle[2])
+                    hi += "9시 방향";
+                msg.obj = hi;
+            }
+            Log.d("Debug :: " , hi+"/"+check);
+//            mMainHandler.sendMessage(msg);
+
+//            try { Thread.sleep(1000); }
+//            catch (InterruptedException e) { e.printStackTrace(); }
+        }
+        // </핸들러메세지>
+        //Log.d("좌표 : ", ""+matInput.rows() + ", " + matInput.cols());
+
+        /*
+        int midX = matInput.rows()/2;
+        int maxY = matInput.cols();
+
+        if(jni_data[0] > 0) {
+            double[] slope = new double[20];
+            for (int i = 0; i < jni_data[0]; i++) {
+                if (jni_data[i * 2 + 1] != midX)
+                    slope[i] = ((double) jni_data[i * 2 + 2] - maxY) / (jni_data[i * 2 + 1] - midX);
+                else
+                    slope[i] = jni_data[i * 2 + 2] - maxY;
+            }
+            Arrays.sort(slope);
+
+            if (slope[0] * slope[jni_data[0] - 1] < 0)
+                Log.d("방향 : ", "전방 전방 전방");
+
+                //debugText.setText("전방에 있다!!!!");
+            else if (slope[0] < 0)
+                Log.d("방향 : ", "오른쪽 오른쪽 오른쪽");
+
+                //debugText.setText("오른쪽에 쏠려 있다!!!!");
+            else
+                Log.d("방향 : ", "전방 전방 전방");
+
+            //debugText.setText("왼쪽에 쏠려 있다!!!!");
+        }
+        */
         return matResult;
     }
+    // </C++ 영상처리 함수 호출 영역>
 
-
-
-    //여기서부턴 퍼미션 관련 메소드
+    // < 카메라 - 수정 X >
+    @Override
+    public void onCameraViewStarted(int width, int height) {}
+    @Override
+    public void onCameraViewStopped() {}
+    // </ 카메라 - 수정 X >
+    // < 퍼미션 - 수정 X >
     static final int PERMISSIONS_REQUEST_CODE = 1000;
     String[] PERMISSIONS  = {"android.permission.CAMERA"};
-
-
     private boolean hasPermissions(String[] permissions) {
         int result;
-
-        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
         for (String perms : permissions){
 
             result = ContextCompat.checkSelfPermission(this, perms);
 
             if (result == PackageManager.PERMISSION_DENIED){
-                //허가 안된 퍼미션 발견
                 return false;
             }
         }
-
-        //모든 퍼미션이 허가되었음
         return true;
     }
-
-
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         switch(requestCode){
@@ -177,11 +267,8 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
     }
-
-
     @TargetApi(Build.VERSION_CODES.M)
     private void showDialogForPermission(String msg) {
-
         AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
         builder.setTitle("알림");
         builder.setMessage(msg);
@@ -198,4 +285,32 @@ public class MainActivity extends AppCompatActivity
         });
         builder.create().show();
     }
+    // </ 퍼미션 - 수정 X >
+    // <액티비티 - 수정 X >
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+        ActivityCompat.finishAffinity(this);
+        System.runFinalizersOnExit(true);
+        System.exit(0);
+    }
+    // </액티비티 - 수정 X >
 }
--- app/src/main/jni/main.cpp
+++ app/src/main/jni/main.cpp
@@ -1,6 +1,6 @@
 #include <jni.h>
 #include "com_example_user_yellowblockdetect_MainActivity.h"
-
+#include <string>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -8,6 +8,8 @@ using namespace cv;
 using namespace std;
 
 extern "C"{
+
+// int 형 string 변환
 string IntToString(int number)
 {
     std::ostringstream convStream;
@@ -15,100 +17,143 @@ string IntToString(int number)
     return convStream.str ();
 }
 
-   JNIEXPORT void JNICALL Java_com_example_user_yellowblockdetect_MainActivity_ConvertRGBtoGray(
-            JNIEnv *env,jobject  instance, jlong matAddrInput,jlong matAddrResult)
-   {
+// 포인트 b에 해당하는 각도값 반환
+int GetAngleABC(Point a, Point b, Point c)
+{
+    Point ab = Point( b.x - a.x, b.y - a.y );
+    Point cb = Point( b.x - c.x, b.y - c.y );
 
-       Mat &img_input = *(Mat *)matAddrInput;
-       Mat &img_result = *(Mat *)matAddrResult;
-       Mat img_hsv, img_gray;
+    float dot = (ab.x * cb.x + ab.y * cb.y);
+    float cross = (ab.x * cb.y - ab.y * cb.x);
 
-       vector<vector<Point> > contours;
+    float alpha = atan2(cross, dot);
 
-       int LowH = 13;//*(255/180);
-       int HighH = 27;
-
-       int LowS = 55;
-       int HighS = 255;
-
-       int LowV = 0;
-       int HighV = 255;
-
-       int filt = 3;
-       int conSize = 600;
-
-       img_result = img_input.clone();
-       cvtColor(img_input, img_hsv, COLOR_RGB2HSV);
-       inRange(img_hsv, Scalar(LowH, LowS, LowV), Scalar(HighH, HighS, HighV), img_gray);
-
-       //morphological opening 작은 점들을 제거
-       erode(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(filt, filt)));
-       dilate(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(filt, filt)));
-       //morphological closing 영역의 구멍 메우기
-       dilate(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(filt, filt)));
-       erode(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(filt, filt)));
-
-       findContours(img_gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-
-       vector<Point2f> approx;
-       for (size_t i = 0; i < contours.size(); i++)
-       {
-           approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
-
-           if (fabs(contourArea(Mat(approx))) > conSize)// && isContourConvex(Mat(approx)) )
-           {
-               int size = approx.size();
-
-               //Contour를 근사화한 직선을 그린다.
-               if (size % 2 == 0) {
-                   line(img_result, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
-
-                   for (int k = 0; k < size - 1; k++)
-                       line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
-
-                   //for (int k = 0; k < size; k++)
-                      // circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-               }
-               else {
-                   line(img_result, approx[0], approx[approx.size() - 1], Scalar(0, 255, 0), 3);
-
-                   for (int k = 0; k < size - 1; k++)
-                       line(img_result, approx[k], approx[k + 1], Scalar(0, 255, 0), 3);
-
-                   //for (int k = 0; k < size; k++)
-                      // circle(img_result, approx[k], 3, Scalar(0, 0, 255));
-               }
-
-               //모든 코너의 각도를 구한다.
-               vector<int> angle;
-               for (int k = 0; k < size; k++)
-               {
-                   Point p0 = approx[k];
-                   Point p1 = approx[(k + 1) % size];
-                   Point p2 = approx[(k + 2) % size];
-
-                   Point ab; //= { (p1.x - p0.x), (p1.y - p0.y) };
-                   ab.x =  (p1.x - p0.x);
-                   ab.y = (p1.y - p0.y);
-                   Point cb; //= { (p1.x - p2.x), (p1.y - p2.y) };
-                   cb.x = (p1.x - p2.x);
-                   cb.y = (p1.y - p2.y);
-
-                   int ang = (int)floor(atan2((ab.x * cb.y - ab.y * cb.x), (ab.x * cb.x + ab.y * cb.y)) * 180.0 / CV_PI + 0.5);
-                   int leng = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
-
-                   Point cen; //= { (p1.x - p2.x), (p1.y - p2.y) };
-                   cen.x =(p1.x+p2.x)/2;
-                   cen.y = (p1.y+p2.y)/2;
-
-                   putText(img_result, IntToString(ang)+'['+IntToString(int(approx[(k + 1) % size].x)) +',' +IntToString(int(approx[(k + 1) % size].y)) + ']', approx[(k + 1) % size], FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0, 0, 255), 1, 8);
-                   putText(img_result, IntToString(leng), cen, FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(255, 0, 0), 1, 8);
-
-
-                   angle.push_back(ang);
-               }
-           }
-       }
-   }
+    return (int)floor(alpha * 180.0 / CV_PI + 0.5); // 세점으로부터 각도 반환
 }
 
+JNIEXPORT jintArray JNICALL Java_com_example_user_yellowblockdetect_MainActivity_ProcessFrame(
+        JNIEnv *env,jobject  instance, jlong matAddrInput,jlong matAddrResult)
+{
+
+    Mat &img_input = *(Mat *)matAddrInput;
+    Mat &img_result = *(Mat *)matAddrResult;
+    Mat img_hsv, img_gray, img_blur;
+
+    int LowH = 20;
+    int HighH = 30;
+
+    int LowS = 50;
+    int HighS = 255;
+
+    int LowV = 50;
+    int HighV = 255;
+
+    int conSize = 500;
+
+    int length = 40;
+    jintArray intArray = env->NewIntArray(length + 2);
+    jint *cintArray = new jint[length + 2];
+    for(int i=0; i<length + 2; i++) {
+        cintArray[i] = 0;
+    }
+
+    int largest_area = 0;
+    int largest_contour_index = 0;
+
+
+    // < 1.블러 처리 >
+    //img_result = img_input.clone();
+   //blur( img_result(Range(0, 100), Range(0, 100)), img_blur, Size( 7, 7 ) );
+    blur( img_input, img_blur, Size( 7, 7 ) );
+//    GaussianBlur(img_input, img_blur, Size(7, 7), 0, 0);
+//    medianBlur(img_input, img_blur, 7);
+    // </ 1.블러 처리 > => img_blur
+
+
+    // < 2.노란색 추출 >
+    cvtColor(img_blur, img_hsv, COLOR_RGB2HSV);
+    inRange(img_hsv, Scalar(LowH, LowS, LowV), Scalar(HighH, HighS, HighV), img_gray);
+    // </ 2.노란색 추출 > => img_gray
+
+
+    // < 3.형태 추출 >
+    vector<Point2f> approx;
+    Rect bounding_rect;
+    vector<vector<Point> > contours;
+
+    //findContours( img_gray, contours, hierarchy,CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE ); // Find the contours in the image
+    findContours(img_gray, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+    // </ 3.형태 추출 > => contours
+
+    if(contours.size() != 0) {
+
+        // < 4.크기가 가장 큰 형태 찾기 >
+        for (size_t i = 0; i < contours.size(); i++) {
+            double a = contourArea(contours[i], false);
+
+            if (a > largest_area && a > conSize) {
+                largest_area = a;
+                largest_contour_index = i;
+                bounding_rect = boundingRect(contours[i]);
+            }
+        }
+        // </ 4.크기가 가장 큰 형태 찾기 > => largest_area,largest_contour_index
+
+        if(largest_area > 0) {
+
+            // < 5.가장 큰 도형의 점을 다각형화 >
+            approxPolyDP(Mat(contours[largest_contour_index]),
+                         approx,
+                         arcLength(Mat(contours[largest_contour_index]), true) * 0.02, true);
+            // </ 5.가장 큰 도형의 점을 다각형화 > => approx
+
+            // < 6.다각형 꼭지점을 최종 반환배열에 저장 >
+            for (int i = 0; i < length / 2; i++) {
+                if (approx.size() <= i) break;
+                cintArray[0]++;
+                cintArray[i * 2 + 1] = approx[i].x;
+                cintArray[i * 2 + 2] = approx[i].y;
+
+                // * img_blur에 꼭지점 좌표를 그린다
+                putText(img_blur, '['+IntToString(int(approx[(i) % length].x)) +',' +IntToString(int(approx[(i) % length].y)) + ']', approx[(i) % length], FONT_HERSHEY_SIMPLEX, 0.5, CV_RGB(0, 0, 255), 1, 8);
+            }
+            // </ 6.다각형 꼭지점을 최종 반환배열에 저장 > => cintArray[0~40]
+
+            // < 7.이미지에 다각형 그리기 >
+            for (int i = 0; i < cintArray[0] - 1; i++) {
+                Point next = Point(cintArray[(i + 1) * 2 + 1], cintArray[(i + 1) * 2 + 2]),
+                        curr = Point(cintArray[i * 2 + 1], cintArray[i * 2 + 2]);
+                line(img_blur, curr, next, Scalar(0, 255, 0), 3);
+            }
+            Point next = Point(cintArray[1], cintArray[2]),
+                    curr = Point(cintArray[(cintArray[0] - 1) * 2 + 1],
+                                 cintArray[(cintArray[0] - 1) * 2 + 2]);
+            line(img_blur, curr, next, Scalar(0, 255, 0), 3);
+            // </ 7.이미지에 다각형 그리기 >
+
+            // < 8. 다각형에 근사한 각도추출 >
+            Vec4f line_obj;
+            fitLine(Mat(contours[largest_contour_index]), line_obj, CV_DIST_L2, 0, 0.01, 0.01);
+            int x0 = line_obj[2];
+            int y0 = line_obj[3];
+            int x1 = x0-200*line_obj[0];
+            int y1 = y0-200*line_obj[1];
+            line(img_blur, Point(x0, y0), Point(x1, y1), Scalar(100,0,200), 3);
+            line(img_blur, Point(x0, y0), Point(x1, y0), Scalar(200,0,100), 3);
+            int angle = GetAngleABC(Point(x0, y0), Point(x1, y1), Point(x1, y0));
+
+            cintArray[length+1] = angle;
+            // </ 8. 다각형에 근사한 각도추출 > => cintArray[41]
+        }
+    }
+
+
+    // < 9. 안드로이드로 반환 데이터 처리 >
+    img_result = img_blur;
+    env->SetIntArrayRegion(intArray, 0, length + 2, cintArray);
+    delete [] cintArray;
+    // </ 9. 안드로이드로 반환 데이터 처리 >
+
+    return intArray;
+}
+}
